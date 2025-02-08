0cba079c304f4950b701b8344644ec1525828731
Rossano <rossanorb@gmail.com>
2025-02-08 19:34:04 -0300

    create migration salesperson

--- src/database/migrations/migrations.ts
+++ src/database/migrations/migrations.ts
@@ -1,5 +1,3 @@
-import { CreateTableSalespersonTable1739027283530 } from "./1739027283530-create_table_salesperson_table";
+import { CreateTableSalespersonTable1739027283530 } from './1739027283530-create_table_salesperson_table';
 
-export const migrations = [
-    CreateTableSalespersonTable1739027283530,
-]
+export const migrations = [CreateTableSalespersonTable1739027283530];
